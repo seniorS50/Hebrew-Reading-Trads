57938f20c1b7d6e8f38d3611429ec8afe3855467
williamtarr <112667077+williamtarr@users.noreply.github.com>
2022-12-03 22:22:49 +0000

    Sat, Dec 3, 2022, 10:22 PM +00:00

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const cities = JSON.parse("/city_locs.json");
-console.log(cities[1]['Latitude'])
+//const json = open('/city_locs.json')
+//const cities = JSON.parse(json);
+//console.log(cities[1]['Latitude'])
+
+import code from './city_locs.json' assert {type: 'json'};
+console.log(code[1]'Latitude')
