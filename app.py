ed7c21d271bf881f819662d7a0633f682ec6c821
seniorS50 <112778112+seniorS50@users.noreply.github.com>
2022-12-19 15:02:14 -0500

    changed the javascript structure

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ app = Flask(__name__)
 with open('filenames3.json') as f:
     data = json.load(f)
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 @app.route("/")
 def index():
     return render_template("index.html", entry=data)
